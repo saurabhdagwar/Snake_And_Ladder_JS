c4bc94933e8565f9bc22aa007f53d725a402ed07
Saurabh Dagwar <saurabhdagwar2020@gmail.com>
2020-10-27 20:52:32 +0530

    add: New function for roll a dice

--- SnakeAndLadder.js
+++ SnakeAndLadder.js
@@ -1,3 +1,9 @@
 //Constants
 const STARTING_POSITION = 0;
 const ENDING_POSITION = 100;
+
+function RollDice(){
+    var random_check = Math.floor( Math.random() * 6 ) + 1 ;
+    console.log("random check: "+random_check );
+}
+RollDice();
