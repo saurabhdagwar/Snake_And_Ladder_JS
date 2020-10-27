de9d38bdb2a899ff1ac16ac6f7b3f4bf6294d3b3
Saurabh Dagwar <saurabhdagwar2020@gmail.com>
2020-10-27 21:22:13 +0530

    add: Check for options and Play Game

--- SnakeAndLadder.js
+++ SnakeAndLadder.js
@@ -1,10 +1,36 @@
 //Constants
 const STARTING_POSITION = 0;
 const ENDING_POSITION = 100;
+const NO_PLAY = 0 ;
+const LADDER = 2 ;
+const SNAKE = 1 ;
 
-function RollDice(){
-    var random_check = Math.floor( Math.random() * 6 ) + 1 ;
-    console.log("random check: "+random_check );
+//Variables
+let currentPosition ;
+
+function GamePlayed(){
+    currentPosition = STARTING_POSITION ;
+    var dice = Math.floor( Math.random() * 6 ) + 1 ;
+    process.stdout.write("random check: "+dice +" |");
+    var checkOption = Math.floor( Math.random() * 3 ) ;
+    switch(checkOption){
+        case LADDER :
+           console.log(' LADDER ');
+            if(currentPosition + dice <= ENDING_POSITION){
+            currentPosition = currentPosition + dice;
+            }
+            break;
+        case SNAKE :
+            console.log(' SNAKE ');
+            if(currentPosition - dice >= STARTING_POSITION ){
+            currentPosition = currentPosition - dice;
+            }
+            break;
+        default:
+            console.log(' NO PLAY ');
+            currentPosition = currentPosition;
+            break;
+    }
+    console.log(`Current Position: ${currentPosition}`);
 }
-
-RollDice();
+GamePlayed();
